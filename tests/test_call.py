54e726d08fc2f371da5a6956fa212b98a19222f4
vhzkclq0705 <vhzkclq0705@naver.com>
2025-03-14 13:40:39 +0900

    pytest로 url 테스트

---
+++ tests/test_call.py
@@ -0,0 +1,14 @@
+import os
+from movies_etl.api.call import gen_url
+
+def test_gen_url_default():
+    r = gen_url()
+    print(r)
+    assert "kobis" in r
+    assert "targetDt" in r
+    assert os.getenv("MOVIES_API_KEY") in r
+
+def test_gen_url_default_with_params():
+    r = gen_url(url_param={"multiMovieYn": "Y", "repNationCd": "K"})
+    assert "&multiMovieYn=Y" in r
+    assert "&repNationCd=K" in r
