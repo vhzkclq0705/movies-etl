d1d578d7758596a618d5b8cab5e900180dd5a3b0
vhzkclq0705 <vhzkclq0705@naver.com>
2025-03-17 15:27:24 +0900

    dotenv 설치 및 버전 업데이트

--- src/movies_etl/api/call.py
+++ src/movies_etl/api/call.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_URL = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
 API_KEY = os.getenv("MOVIES_API_KEY")
